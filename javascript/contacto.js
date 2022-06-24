c3883a750c28ea7cc8aa0e9a373fc829d910b219
Joaquin Gimenez <js.gimenez98@gmail.com>
2022-06-23 22:52:33 -0300

    Evento agregado en contacto.js

---
+++ javascript/contacto.js
@@ -0,0 +1,8 @@
+function validarFormulario (formulario) {
+    let miFormulario = document.getElementById ("formulario");
+    miFormulario.addEventListener ("submit", (e) => {
+        e.preventDefault ();
+        let nombre = document.getElementById ('Nombre').value
+            console.log (nombre);
+    })
+}
