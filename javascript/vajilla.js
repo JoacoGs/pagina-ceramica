d1e919126751fe0a587eb98cd2ef4d3f5edfd70f
Joaquin Gimenez <js.gimenez98@gmail.com>
2022-07-13 21:43:10 -0300

    archivo json con datos de productos agregado

--- javascript/vajilla.js
+++ javascript/vajilla.js
@@ -136,3 +136,21 @@ document.addEventListener("keyup", (e) => {
     });
   }
 });
+
+const lista = document.querySelectorAll("#listado");
+
+fetch("/data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((producto) => {
+      const li = document.createElement("li");
+      li.innerHTML = `
+        <h4>${producto.nombre}</h4>
+        <p>${producto.precio}</p>
+        <p>Codigo: ${producto.id}</p>
+        <hr/>
+        `;
+
+      lista.append(li);
+    });
+  });
